64ccc78b8269a7a95cdf3cd5816932ce4ceb2a22
Ivan <bandola.ivan@gmail.com>
2021-10-21 20:44:07 +0200

    Admin - basic company list

--- frontend/admin/src/App.tsx
+++ frontend/admin/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Admin, Resource, ListGuesser } from 'react-admin'
+import jsonServerProvider from 'ra-data-json-server'
 
-const App: React.FC = () => {
-  return (
-    <div className="App" id="app">
-      Starter app
-    </div>
-  )
-}
+import { CompanyInfo, CompanyList } from './components/companies'
+
+const dataProvider = jsonServerProvider(
+  'https://my-json-server.typicode.com/Bandius/myJsonServer'
+)
+
+const App: React.FC = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="companies" list={CompanyList} show={CompanyInfo} />
+    <Resource name="users" list={ListGuesser} />
+  </Admin>
+)
 
 export default App
