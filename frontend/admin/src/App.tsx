d9d90ffcb507a1542fee19e30f0706af83799c46
Ivan <bandola.ivan@gmail.com>
2021-10-23 22:34:13 +0200

    Added auth provider and login
    currently allows all combinations of username and password

--- frontend/admin/src/App.tsx
+++ frontend/admin/src/App.tsx
@@ -4,13 +4,14 @@ import jsonServerProvider from 'ra-data-json-server'
 
 import { CompanyShow, CompanyList } from './components/Companies'
 import { UserList } from './components/Users'
+import authProvider from './components/authProvider'
 
 const dataProvider = jsonServerProvider(
   'https://my-json-server.typicode.com/Bandius/myJsonServer'
 )
 
 const App: React.FC = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dataProvider={dataProvider} authProvider={authProvider}>
     <Resource name="companies" list={CompanyList} show={CompanyShow} />
     <Resource name="users" list={UserList} />
   </Admin>
---
+++ frontend/admin/src/components/authProvider.tsx
@@ -0,0 +1,23 @@
+export default {
+  login: ({ username }: { username: string }) => {
+    localStorage.setItem('username', username)
+    return Promise.resolve()
+  },
+  logout: () => {
+    localStorage.removeItem('username')
+    return Promise.resolve()
+  },
+  checkError: ({ status }: { status: number }) => {
+    if (status === 401 || status === 403) {
+      localStorage.removeItem('username')
+      return Promise.reject()
+    }
+    return Promise.resolve()
+  },
+  checkAuth: () => {
+    return localStorage.getItem('username')
+      ? Promise.resolve()
+      : Promise.reject()
+  },
+  getPermissions: () => Promise.resolve(),
+}
