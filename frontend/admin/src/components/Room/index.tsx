d6a54d80b7389019687fe103d5d553be8e47227d
Ivan <bandola.ivan@gmail.com>
2021-11-08 12:19:03 +0100

    Add rooms to superadmin

--- frontend/admin/src/components/Room/index.tsx
+++ frontend/admin/src/components/Room/index.tsx
@@ -1,5 +1,9 @@
 import RoomList from './RoomList'
+import RoomEdit from './RoomEdit'
+import RoomCreate from './RoomCreate'
 
 export default {
   list: RoomList,
+  edit: RoomEdit,
+  create: RoomCreate,
 }
