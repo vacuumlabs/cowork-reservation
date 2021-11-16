ffd02bc484afe1a57c9b374e27db975ab60f3c84
matusziak <matus.ziak@vacuumlabs.com>
2021-11-16 21:13:15 +0100

    Add temp idToken console.log

--- frontend/admin/src/App.tsx
+++ frontend/admin/src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Admin, Resource } from 'react-admin'
 import jsonServerProvider from 'ra-data-json-server'
 
-import authProvider from './authProvider'
+import authProvider, { getIdToken } from './authProvider'
 import Tenant from './components/Tenant'
 import User from './components/User'
 import Room from './components/Room'
@@ -13,50 +13,64 @@ const dataProvider = jsonServerProvider(
   'https://my-json-server.typicode.com/Bandius/myJsonServer'
 )
 
-const App: React.FC = () => (
-  <Admin
-    dataProvider={dataProvider}
-    authProvider={authProvider}
-    layout={Layout}
-  >
-    {(permissions) =>
-      permissions === UserRole.SUPER_ADMIN
-        ? [
-            <Resource
-              key="tenants"
-              name="tenants"
-              {...Tenant}
-              options={{ label: 'Tenants' }}
-            />,
-            <Resource
-              key="users"
-              name="users"
-              {...User}
-              options={{ label: 'Tenant Admins' }}
-            />,
-            <Resource
-              key="rooms"
-              name="rooms"
-              {...Room}
-              options={{ label: 'Rooms' }}
-            />,
-          ]
-        : [
-            <Resource
-              key="tenants"
-              name="tenants"
-              show={Tenant.show}
-              options={{ label: 'Profile' }}
-            />,
-            <Resource
-              key="users"
-              name="users"
-              {...User}
-              options={{ label: 'Tenant Admins' }}
-            />,
-          ]
+const App: React.FC = () => {
+  // TEMP console.log firebase idToken for dev purposes
+  useEffect(() => {
+    const logIdToken = async () => {
+      // eslint-disable-next-line no-console
+      console.log(await getIdToken())
     }
-  </Admin>
-)
+
+    if (process.env.NODE_ENV === 'development') {
+      logIdToken()
+    }
+  }, [])
+
+  return (
+    <Admin
+      dataProvider={dataProvider}
+      authProvider={authProvider}
+      layout={Layout}
+    >
+      {(permissions) =>
+        permissions === UserRole.SUPER_ADMIN
+          ? [
+              <Resource
+                key="tenants"
+                name="tenants"
+                {...Tenant}
+                options={{ label: 'Tenants' }}
+              />,
+              <Resource
+                key="users"
+                name="users"
+                {...User}
+                options={{ label: 'Tenant Admins' }}
+              />,
+              <Resource
+                key="rooms"
+                name="rooms"
+                {...Room}
+                options={{ label: 'Rooms' }}
+              />,
+            ]
+          : [
+              <Resource
+                key="tenants"
+                name="tenants"
+                show={Tenant.show}
+                options={{ label: 'Profile' }}
+              />,
+              <Resource
+                key="users"
+                name="users"
+                {...User}
+                options={{ label: 'Tenant Admins' }}
+              />,
+            ]
+      }
+    </Admin>
+  )
+}
 
 export default App
