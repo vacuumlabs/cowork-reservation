b4202e3a15d509910664839502f4e6c6534ae276
matusziak <matus.ziak@vacuumlabs.com>
2021-11-19 14:53:12 +0100

    Add navigation and basic screens

--- frontend/tablet/src/screens/RoomDetailScreen/index.tsx
+++ frontend/tablet/src/screens/RoomDetailScreen/index.tsx
@@ -1,11 +1,22 @@
+import { StackScreenProps } from '@react-navigation/stack'
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 
+import { NavigatorStackParamList } from '..'
 import { Typography, Screen } from '../../components'
 import Header from './Header'
 import Footer from './Footer'
 
-const RoomDetailScreen: React.FC = () => {
+type RoomDetailProps = StackScreenProps<
+  NavigatorStackParamList,
+  'RoomDetailScreen'
+>
+
+const RoomDetailScreen: React.FC<RoomDetailProps> = ({
+  route,
+}: RoomDetailProps) => {
+  const { room } = route.params
+
   return (
     <Screen layoutVariant="space-between">
       <View style={styles.header}>
@@ -14,7 +25,7 @@ const RoomDetailScreen: React.FC = () => {
 
       <View style={styles.main}>
         {/* TODO add margins */}
-        <Typography variant="h1">Matrix</Typography>
+        <Typography variant="h1">{room.title}</Typography>
         <Typography variant="h3">FREE</Typography>
         <Typography variant="h2">00:22:35</Typography>
       </View>
--- frontend/tablet/src/screens/index.tsx
+++ frontend/tablet/src/screens/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
+import { createStackNavigator, StackScreenProps } from '@react-navigation/stack'
 
+import { Room } from '../models'
+import LoginScreen from './LoginScreen'
+import RoomListScreen from './RoomListScreen'
 import RoomDetailScreen from './RoomDetailScreen'
 
 const { Navigator, Screen } = createStackNavigator()
 
+export type NavigatorStackParamList = {
+  LoginScreen: undefined
+  RoomListScreen: undefined
+  RoomDetailScreen: {
+    room: Room
+  }
+}
+
+export type NavigationProps = StackScreenProps<
+  NavigatorStackParamList,
+  keyof NavigatorStackParamList
+>
+
 const ScreenNavigator: () => JSX.Element = () => {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
+      <Screen name="LoginScreen" component={LoginScreen} />
+      <Screen name="RoomListScreen" component={RoomListScreen} />
       <Screen name="RoomDetailScreen" component={RoomDetailScreen} />
     </Navigator>
   )
