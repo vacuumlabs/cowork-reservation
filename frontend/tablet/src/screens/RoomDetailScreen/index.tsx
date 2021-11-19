16a0bdd12af46f4510df8c979f0d1b0088e61a6a
matusziak <matus.ziak@vacuumlabs.com>
2021-11-19 14:52:48 +0100

    Refactor layout to be reusable by more screens

--- frontend/tablet/src/screens/RoomDetailScreen/index.tsx
+++ frontend/tablet/src/screens/RoomDetailScreen/index.tsx
@@ -1,28 +1,26 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 
-import { Typography, Screen, theme } from '../../components'
+import { Typography, Screen } from '../../components'
 import Header from './Header'
 import Footer from './Footer'
 
 const RoomDetailScreen: React.FC = () => {
   return (
-    <Screen>
-      <View style={styles.layout}>
-        <View style={styles.header}>
-          <Header />
-        </View>
+    <Screen layoutVariant="space-between">
+      <View style={styles.header}>
+        <Header />
+      </View>
 
-        <View style={styles.main}>
-          {/* TODO add margins */}
-          <Typography variant="h1">Matrix</Typography>
-          <Typography variant="h3">FREE</Typography>
-          <Typography variant="h2">00:22:35</Typography>
-        </View>
+      <View style={styles.main}>
+        {/* TODO add margins */}
+        <Typography variant="h1">Matrix</Typography>
+        <Typography variant="h3">FREE</Typography>
+        <Typography variant="h2">00:22:35</Typography>
+      </View>
 
-        <View style={styles.footer}>
-          <Footer />
-        </View>
+      <View style={styles.footer}>
+        <Footer />
       </View>
     </Screen>
   )
@@ -31,14 +29,9 @@ const RoomDetailScreen: React.FC = () => {
 export default RoomDetailScreen
 
 const styles = StyleSheet.create({
-  layout: {
-    flex: 1,
-    justifyContent: 'space-between',
-  },
   header: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    padding: theme.spacing.xl * 2,
   },
   main: {
     justifyContent: 'center',
@@ -47,6 +40,5 @@ const styles = StyleSheet.create({
   footer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    padding: theme.spacing.xl * 2,
   },
 })
