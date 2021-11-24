f275401f24c8b2ba113b53b3e6f50e1f8c05f6f7
Ivan <bandola.ivan@gmail.com>
2021-11-24 08:41:59 +0100

    Room create with choice autocomplete

--- frontend/admin/src/components/Room/RoomCreate.tsx
+++ frontend/admin/src/components/Room/RoomCreate.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   CreateProps,
   Create,
@@ -9,34 +9,38 @@ import {
   AutocompleteInput,
 } from 'react-admin'
 
-const RoomCreate: (props: CreateProps) => JSX.Element = (props) => (
-  <Create {...props}>
-    <SimpleForm>
-      <TextInput source="name" validate={required()} />
-      <ReferenceInput
-        source="cityId"
-        reference="cities"
-        link={false}
-        validate={required()}
-        label="City"
-      >
-        <AutocompleteInput />
-      </ReferenceInput>
-      <ReferenceInput
-        source="buildingId"
-        reference="buildings"
-        link={false}
-        validate={required()}
-        label="Building"
-      >
-        <AutocompleteInput />
-      </ReferenceInput>
+const RoomCreate: (props: CreateProps) => JSX.Element = (props) => {
+  const [city, setCity] = useState('')
+  return (
+    <Create {...props}>
+      <SimpleForm>
+        <TextInput source="name" validate={required()} />
+        <ReferenceInput
+          source="cityId"
+          reference="cities"
+          link={false}
+          validate={required()}
+          label="City"
+        >
+          <AutocompleteInput onInputValueChange={(value) => setCity(value)} />
+        </ReferenceInput>
+        <ReferenceInput
+          source="buildingId"
+          reference="buildings"
+          link={false}
+          validate={required()}
+          label="Building"
+          filter={{ cityId: city }}
+        >
+          <AutocompleteInput />
+        </ReferenceInput>
 
-      <TextInput source="floor" validate={required()} />
-      <TextInput source="capacity" validate={required()} />
-      <TextInput source="equipment" validate={required()} />
-    </SimpleForm>
-  </Create>
-)
+        <TextInput source="floor" validate={required()} />
+        <TextInput source="capacity" validate={required()} />
+        <TextInput source="equipment" validate={required()} />
+      </SimpleForm>
+    </Create>
+  )
+}
 
 export default RoomCreate
