aac2f62d691f4243c6dbb7814bfe3114c0c4df75
MichaelJ-T <59335490+MichaelJ-T@users.noreply.github.com>
2021-11-29 15:14:12 +0100

    Added: calendar database methods

--- backend/app/daos.py
+++ backend/app/daos.py
@@ -1,4 +1,5 @@
 from app.models import Tenant
+from app.models import Calendar
 from app import db
 
 session = db.session
@@ -28,3 +29,43 @@ class TenantDAO:
 
 
 tenant_dao = TenantDAO(Tenant)
+
+class CalendarDAO:
+    def __init__(self, model):
+        self.model = model
+
+    def get_all(self) -> list:
+        return self.to_array(session.query(self.model).all())
+
+    def get(self, kargs: dict):
+        results = session.query(self.model).filter_by(**kargs).all()
+        return self.to_array(results) 
+
+    def add_calendar(self, tenant_id: int, name: str, google_id: str) -> Calendar:
+        new_calendar = Calendar(tenant_id=tenant_id, name=name, google_id=google_id)
+        session.add(new_calendar)
+        session.commit()
+        return new_calendar
+
+    def delete_calendar(self, id: int):
+        try:
+            calendar = session.query(self.model).filter_by(id=id).first()
+            session.delete(calendar)
+            session.commit()
+        except:
+            return False
+        return True
+
+    def to_array(self, results) -> list:
+        return [
+            {
+                "id": row.id,
+                "tenant_id": row.tenant_id, 
+                "name": row.name, 
+                "google_id": row.google_id
+            } 
+            for row in results 
+        ]
+
+
+calendar_dao = CalendarDAO(Calendar)
