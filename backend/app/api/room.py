a4ca76dcb5f731c5c29b3584bf7b10f83dac6955
MichaelJ-T <59335490+MichaelJ-T@users.noreply.github.com>
2021-12-12 01:13:23 +0100

    Refactored get_room_list for react admin

--- backend/app/api/room.py
+++ backend/app/api/room.py
@@ -1,5 +1,5 @@
 import json
-from flask import jsonify, request, render_template
+from flask import jsonify, request, make_response, render_template
 from flask.blueprints import Blueprint
 from app.daos import room_dao
 
@@ -8,17 +8,34 @@ room_bp = Blueprint("room_bp", __name__)
 @room_bp.route("/rooms", methods=["GET"])
 def get_room_list():
     # TODO: check if tenant has permissions to view all rooms
-    url_args = request.args.to_dict()
+    url_args = request.args
     filters = {}
     sort = []
     results_range = []
-    if "filter" in url_args:
-        filters = json.loads(url_args["filter"])
-    if "sort" in url_args:
-        sort = json.loads(url_args["sort"])
-    if "range" in url_args:
-        results_range = json.loads(url_args["range"])
-    return jsonify(room_dao.get_all(filters, sort, results_range))
+    for key, value in url_args.items(multi=True):
+        if key == "id":
+            if "id" in filters:
+                filters['id'].append(int(value))
+            else:
+                filters['id'] = [int(value)]
+        elif key == "_sort":
+            sort.insert(0,str(value))
+        elif key == "_order":
+            sort.insert(1,str(value))
+        elif key == "_start":
+            results_range.insert(0,int(value))
+        elif key == "_end":
+            results_range.insert(1,int(value))
+        else:
+            if key in filters:
+                filters[key].append(value)
+            else:
+                filters[key] = [value]
+    results = room_dao.get_all(filters, sort, results_range)
+    resp = make_response(jsonify(results['data']), 200)
+    resp.headers['Access-Control-Expose-Headers'] = 'X-Total-Count'
+    resp.headers['X-Total-Count'] = results['count']
+    return resp
 
 @room_bp.route("/rooms/<id>", methods=["GET"])
 def get_room_one(id):
