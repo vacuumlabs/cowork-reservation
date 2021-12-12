c2e283cc99046aa7e9218d2d0b433bb88cd94f28
MichaelJ-T <59335490+MichaelJ-T@users.noreply.github.com>
2021-12-12 01:10:59 +0100

    Renamed api endpoints in room.py

--- backend/app/api/room.py
+++ backend/app/api/room.py
@@ -5,7 +5,7 @@ from app.daos import room_dao
 
 room_bp = Blueprint("room_bp", __name__)
 
-@room_bp.route("/room", methods=["GET"])
+@room_bp.route("/rooms", methods=["GET"])
 def get_room_list():
     # TODO: check if tenant has permissions to view all rooms
     url_args = request.args.to_dict()
@@ -20,23 +20,23 @@ def get_room_list():
         results_range = json.loads(url_args["range"])
     return jsonify(room_dao.get_all(filters, sort, results_range))
 
-@room_bp.route("/room/<id>", methods=["GET"])
+@room_bp.route("/rooms/<id>", methods=["GET"])
 def get_room_one(id):
     #TODO: check if tenant has permissions to view desired room
     return jsonify(room_dao.get_one(id))
 
-@room_bp.route("/room/<id>", methods=["PUT"])
+@room_bp.route("/rooms/<id>", methods=["PUT"])
 def update_room(id):
     # TODO: check if tenant has permissions to update desired room
     data = request.json
     return jsonify(room_dao.update_room(id, data))
 
-@room_bp.route("/room/<id>", methods=["DELETE"])
+@room_bp.route("/rooms/<id>", methods=["DELETE"])
 def delete_room(id):
     room_dao.delete_room(id)
     return jsonify(room_dao.get_all())
 
-@room_bp.route("/room", methods=["POST"])
+@room_bp.route("/rooms", methods=["POST"])
 def create_room():
     try:
         data = request.json
