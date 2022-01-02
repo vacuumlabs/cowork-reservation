1f793663e58cd7ada496d94caf1214988b2503ad
matusziak <matus.ziak@vacuumlabs.com>
2022-01-02 11:41:51 +0100

    Simpify renders

--- frontend/admin/src/components/Menu.tsx
+++ frontend/admin/src/components/Menu.tsx
@@ -15,49 +15,61 @@ import {
 
 import { User, UserRole } from '../models'
 
+const useMenuItems = (user: User) => ({
+  [UserRole.SUPER_ADMIN]: [
+    <MenuItemLink
+      key="tenants"
+      to="/tenants"
+      primaryText="Tenants"
+      leftIcon={<TenantIcon />}
+    />,
+    <MenuItemLink
+      key="users"
+      to="/users"
+      primaryText="Tenant Admins"
+      leftIcon={<UserIcon />}
+    />,
+    <MenuItemLink
+      key="rooms"
+      to="/rooms"
+      primaryText="Rooms"
+      leftIcon={<RoomIcon />}
+    />,
+  ],
+  [UserRole.TENANT_ADMIN]: [
+    <MenuItemLink
+      key="tenants"
+      to={user ? `/tenants/${(user as User).tenantId}/show` : ''}
+      primaryText="My Profile"
+      leftIcon={<ProfileIcon />}
+    />,
+    <MenuItemLink
+      key="users"
+      to="/users"
+      primaryText="My Admins"
+      leftIcon={<UserIcon />}
+    />,
+  ],
+  [UserRole.USER]: [
+    <MenuItemLink
+      key="tenants"
+      to={user ? `/tenants/${user.tenantId}/show` : ''}
+      primaryText="My Profile"
+      leftIcon={<ProfileIcon />}
+    />,
+  ],
+})
+
 const _Menu: (props: MenuProps) => JSX.Element = (props) => {
   const { permissions } = usePermissions()
   const { identity } = useGetIdentity()
+  const menuItems = useMenuItems(identity as User)
 
   return (
     <Menu {...props}>
-      {permissions === UserRole.SUPER_ADMIN
-        ? [
-            <MenuItemLink
-              key="tenants"
-              to="/tenants"
-              primaryText="Tenants"
-              leftIcon={<TenantIcon />}
-            />,
-            <MenuItemLink
-              key="users"
-              to="/users"
-              primaryText="Tenant Admins"
-              leftIcon={<UserIcon />}
-            />,
-            <MenuItemLink
-              key="rooms"
-              to="/rooms"
-              primaryText="Rooms"
-              leftIcon={<RoomIcon />}
-            />,
-          ]
-        : [
-            <MenuItemLink
-              key="tenants"
-              to={
-                identity ? `/tenants/${(identity as User).tenantId}/show` : ''
-              }
-              primaryText="My Profile"
-              leftIcon={<ProfileIcon />}
-            />,
-            <MenuItemLink
-              key="users"
-              to="/users"
-              primaryText="My Admins"
-              leftIcon={<UserIcon />}
-            />,
-          ]}
+      {permissions in UserRole
+        ? menuItems[permissions as UserRole]
+        : menuItems[UserRole.USER]}
     </Menu>
   )
 }
