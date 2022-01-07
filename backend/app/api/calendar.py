8cb503b2ea7ced8848df30c77dd88b3df849b5df
MichaelJ-T <59335490+MichaelJ-T@users.noreply.github.com>
2022-01-07 17:46:36 +0100

    Changed get multiple calendar endpoint

--- backend/app/api/calendar.py
+++ backend/app/api/calendar.py
@@ -1,21 +1,42 @@
-from flask import jsonify, request, render_template
+from flask import jsonify, request, make_response, render_template
 from flask.blueprints import Blueprint
 from app.daos import calendar_dao
 
 calendar_bp = Blueprint("calendar_bp", __name__)
 
-@calendar_bp.route("/calendar/", methods=["GET"])
-def get_calendar():
-    #TODO: check if tenant has permissions to view all calendars
-    urlArgs = request.args.to_dict()
-    queryArgs = {}
-    if "id" in urlArgs: queryArgs['id'] = urlArgs['id']
-    if "tenant_id" in urlArgs: queryArgs['tenant_id'] = urlArgs['tenant_id']
-    if "name" in urlArgs: queryArgs['name'] = urlArgs['name']
-    if queryArgs:
-        return jsonify(calendar_dao.get(queryArgs))
-    else:
-        return jsonify(calendar_dao.get_all())
+
+@calendar_bp.route("/calendars/", methods=["GET"])
+def get_calendar_list():
+    # TODO: check if tenant has permissions to view all calendars
+    url_args = request.args
+    filters = {}
+    sort = []
+    results_range = []
+    for key, value in url_args.items(multi=True):
+        if key == "id":
+            if "id" in filters:
+                filters["id"].append(int(value))
+            else:
+                filters["id"] = [int(value)]
+        elif key == "_sort":
+            sort.insert(0, str(value))
+        elif key == "_order":
+            sort.insert(1, str(value))
+        elif key == "_start":
+            results_range.insert(0, int(value))
+        elif key == "_end":
+            results_range.insert(1, int(value))
+        else:
+            if key in filters:
+                filters[key].append(value)
+            else:
+                filters[key] = [value]
+    results = calendar_dao.get_all(filters, sort, results_range)
+    resp = make_response(jsonify(results["data"]), 200)
+    resp.headers["Access-Control-Expose-Headers"] = "X-Total-Count"
+    resp.headers["X-Total-Count"] = results["count"]
+    return resp
+
 
 @calendar_bp.route("/calendars/<id>", methods=["DELETE"])
 def delete_calendar(id):
