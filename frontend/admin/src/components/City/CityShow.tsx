95b9333f1b87c260f88fef4bb3da00d06cc49630
Ivan <bandola.ivan@gmail.com>
2022-01-14 13:35:27 +0100

    City show component

---
+++ frontend/admin/src/components/City/CityShow.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {
+  ShowProps,
+  Show,
+  SimpleShowLayout,
+  TextField,
+  ReferenceManyField,
+  Datagrid,
+  EditButton,
+} from 'react-admin'
+import { City } from 'shared/models'
+
+const CityTitle = ({ record }: { record?: City }) => (
+  <span>{record ? record.name : 'City'}</span>
+)
+
+const CityShow: (props: ShowProps) => JSX.Element = (props) => (
+  <Show title={<CityTitle />} {...props}>
+    <SimpleShowLayout>
+      <ReferenceManyField
+        fullWidth
+        label="Buildings"
+        reference="buildings"
+        target="cityId"
+        sort={{ field: 'id', order: 'ASC' }}
+      >
+        <Datagrid rowClick="show">
+          <TextField source="name" />
+          <TextField source="address" />
+          <EditButton />
+        </Datagrid>
+      </ReferenceManyField>
+    </SimpleShowLayout>
+  </Show>
+)
+
+export default CityShow
