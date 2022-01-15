4d1d3851643b95da022fac8a199f731f3d3f56fd
Dávid Varga <david.varga.7@student.tuke.sk>
2022-01-15 16:59:31 +0100

    add mock Building, Event, City data to RoomListScreen

--- frontend/tablet/src/screens/RoomListScreen/index.tsx
+++ frontend/tablet/src/screens/RoomListScreen/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet } from 'react-native'
-import { Room } from 'shared/models'
+import { Room, Building, Event, City } from 'shared/models'
 
 import { NavigationProps } from '..'
 import authProvider from '../../authProvider'
@@ -8,39 +8,92 @@ import { Button, Grid, Screen } from '../../components'
 import RoomCard from './RoomCard'
 
 // TODO use real data
+const dummyCityList: City[] = [
+  {
+    id: '1',
+    name: 'Bratislava',
+  },
+]
+
+const dummyBuildingList: Building[] = [
+  {
+    id: '1',
+    name: 'SkyPark',
+    cityId: dummyCityList[0].id,
+    city: dummyCityList[0],
+    address: 'Jurkovičova Tepláreň',
+  },
+]
+
+const dummyEventList: Event[] = [
+  {
+    calendarId: '1',
+    roomId: '1',
+    name: 'VC Meeting',
+    author: 'Karol Sloboda',
+    start: '2022-01-15T16:33:54.795Z',
+    end: '2022-01-15T17:03:54.795Z',
+    googleId: '1',
+    tenantId: '1',
+  },
+  {
+    calendarId: '2',
+    roomId: '1',
+    name: 'VC Meeting 2',
+    author: 'Janko Zelenka',
+    start: '2022-01-15T18:45:00.795Z',
+    end: '2022-01-15T19:00:00.795Z',
+    googleId: '2',
+    tenantId: '2',
+  },
+  {
+    calendarId: '3',
+    roomId: '2',
+    name: 'TLK Meeting',
+    author: 'Milan Hevier',
+    start: '2022-01-16T11:00:00.795Z',
+    end: '2022-01-16T12:00:00.795Z',
+    googleId: '3',
+    tenantId: '3',
+  },
+]
+
 const dummyRoomList: Room[] = [
   {
     id: '1',
     name: 'Matrix',
-    city: 'Bratislava',
-    cityId: '1',
-    building: 'SkyPark',
-    buildingId: '1',
+    city: dummyCityList[0],
+    cityId: dummyCityList[0].id,
+    building: dummyBuildingList[0],
+    buildingId: dummyBuildingList[0].id,
     floor: '7',
     capacity: '4',
     equipment: '',
+    event: dummyEventList,
   },
   {
     id: '2',
     name: 'Harry Potter',
-    city: 'Bratislava',
-    cityId: '1',
-    building: 'SkyPark',
-    buildingId: '1',
+    city: dummyCityList[0],
+    cityId: dummyCityList[0].id,
+    building: dummyBuildingList[0],
+    buildingId: dummyBuildingList[0].id,
     floor: '7',
     capacity: '2',
     equipment: '',
+    event: dummyEventList,
   },
   {
     id: '3',
     name: 'Tardis',
-    city: 'Bratislava',
-    cityId: '1',
-    building: 'SkyPark',
-    buildingId: '1',
+    city: dummyCityList[0],
+    cityId: dummyCityList[0].id,
+    building: dummyBuildingList[0],
+    buildingId: dummyBuildingList[0].id,
     floor: '7',
     capacity: '1',
     equipment: '',
+    event: dummyEventList,
   },
 ]
 
