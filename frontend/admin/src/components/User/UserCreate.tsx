612d34fc22e74237969febf06514fd94e85a372e
Ivan <bandola.ivan@gmail.com>
2022-01-17 11:18:53 +0100

    Create component

--- frontend/admin/src/components/User/UserCreate.tsx
+++ frontend/admin/src/components/User/UserCreate.tsx
@@ -7,22 +7,31 @@ import {
   ReferenceInput,
   AutocompleteInput,
   required,
+  useGetIdentity,
 } from 'react-admin'
 
 const UserCreate: (props: CreateProps) => JSX.Element = (props) => {
+  const { identity } = useGetIdentity()
   return (
     <Create title="Add Admin" {...props}>
       <SimpleForm>
-        <TextInput source="name" validate={required()} />
         <TextInput source="email" validate={required()} />
         <ReferenceInput
           source="tenantId"
           reference="tenants"
           link={false}
-          validate={required()}
+          defaultValue={identity?.tenantId}
+          disabled
         >
           <AutocompleteInput />
         </ReferenceInput>
+        <AutocompleteInput
+          source="role"
+          choices={[
+            { id: 'TENANT_ADMIN', name: 'Tenant admin' },
+            { id: 'USER', name: 'Employee' },
+          ]}
+        />
       </SimpleForm>
     </Create>
   )
