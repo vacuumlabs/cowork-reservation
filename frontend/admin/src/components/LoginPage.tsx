b69078605fbd76ade9d9e761756097b5ce76fcd9
Ivan <bandola.ivan@gmail.com>
2022-01-17 18:25:17 +0100

    Simple registration
    WIP

--- frontend/admin/src/components/LoginPage.tsx
+++ frontend/admin/src/components/LoginPage.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 import { Login } from 'ra-ui-materialui'
+import { Button, Card, TextField } from '@mui/material'
 
 // TODO extract to env
 const firebaseConfig = {
@@ -23,17 +24,119 @@ const uiConfig = {
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
   ],
+  popupMode: true,
 }
 
 function LoginPage(): JSX.Element {
+  const [loginPage, setLoginPage] = useState(true)
+
+  const handleSwitch = () => {
+    setLoginPage(!loginPage)
+  }
+
+  // TODO
+  const handleRegistration = () => {
+    console.log('NYI (not yet implemented)')
+  }
   return (
     <div>
-      <Login>
-        <StyledFirebaseAuth
-          uiConfig={uiConfig}
-          firebaseAuth={firebase.auth()}
-        />
-      </Login>
+      {loginPage ? (
+        <Login>
+          <StyledFirebaseAuth
+            uiConfig={uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+          <Button
+            variant="contained"
+            onClick={handleSwitch}
+            style={{
+              display: 'flex',
+              alignSelf: 'center',
+              marginLeft: '7.5em',
+              marginBottom: '1em',
+              backgroundColor: '#3f51b5',
+              fontFamily: 'Roboto,Helvetica,Arial,sans-serif',
+              borderRadius: '2px',
+            }}
+          >
+            Register
+          </Button>
+        </Login>
+      ) : (
+        <Login>
+          <Card
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <TextField
+              required
+              id="standard-required"
+              label="Name"
+              variant="standard"
+              inputProps={{ maxLength: 50 }}
+            />
+            <TextField
+              required
+              id="standard-required"
+              label="E-mail"
+              variant="standard"
+              inputProps={{ maxLength: 50 }}
+            />
+            <TextField
+              required
+              id="standard-password-input"
+              label="Password"
+              type="password"
+              variant="standard"
+            />
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'flex-end',
+              }}
+            >
+              <Button
+                variant="contained"
+                style={{
+                  boxShadow: 'none',
+                  marginTop: 10,
+                  marginBottom: 10,
+                  marginRight: 1,
+                  backgroundColor: 'rgba(0, 0, 255, 0)',
+                  color: '#3f51b5',
+                  fontFamily: 'Roboto,Helvetica,Arial,sans-serif',
+                  borderRadius: '2px',
+                  marginLeft: 20,
+                }}
+                onClick={handleSwitch}
+                size="small"
+              >
+                Cancel
+              </Button>
+              <Button
+                variant="contained"
+                style={{
+                  marginTop: 10,
+                  marginBottom: 10,
+                  marginLeft: 5,
+                  backgroundColor: '#3f51b5',
+                  fontFamily: 'Roboto,Helvetica,Arial,sans-serif',
+                  borderRadius: '2px',
+                  alignSelf: 'flex-end',
+                }}
+                onClick={handleRegistration}
+                size="small"
+              >
+                Register
+              </Button>
+            </div>
+          </Card>
+        </Login>
+      )}
     </div>
   )
 }
