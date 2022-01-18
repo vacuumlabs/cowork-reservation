a3460e453f324f2b5b2a50726e69c5fac7cadc31
MichaelJ-T <59335490+MichaelJ-T@users.noreply.github.com>
2022-01-18 13:06:37 +0100

    Moved parameter deconstruction api->services

--- backend/app/api/tenant.py
+++ backend/app/api/tenant.py
@@ -3,6 +3,7 @@ from flask import jsonify, request, make_response, render_template
 from flask.blueprints import Blueprint
 from app.daos import tenant_dao
 from app.firebase_utils import have_claims
+from app.services import tenant_service
 
 tenant_bp = Blueprint("tenant_bp", __name__)
 
@@ -39,29 +40,12 @@ def del_tenant(id):
 def get_tenant_list():
     if have_claims(request.cookies.get("login_token")):
         url_args = request.args
-        filters = {}
-        sort = []
-        results_range = []
-        for key, value in url_args.items(multi=True):
-            if key == "id":
-                if "id" in filters:
-                    filters['id'].append(int(value))
-                else:
-                    filters['id'] = [int(value)]
-            elif key == "_sort":
-                sort.insert(0,str(value))
-            elif key == "_order":
-                sort.insert(1,str(value))
-            elif key == "_start":
-                results_range.insert(0,int(value))
-            elif key == "_end":
-                results_range.insert(1,int(value))
-            else:
-                if key in filters:
-                    filters[key].append(value)
-                else:
-                    filters[key] = [value]
-        results = tenant_dao.get_all(filters, sort, results_range)
+        params = tenant_service.url_args_to_query_params_dict(url_args)
+        results = tenant_dao.get_all(
+            params['filters'],
+            params['sort'], 
+            params['range']
+            )
         resp = make_response(jsonify(results['data']), 200)
         resp.headers['Access-Control-Expose-Headers'] = 'X-Total-Count'
         resp.headers['X-Total-Count'] = results['count']
