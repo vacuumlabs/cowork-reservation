dabfd04981c1ef2f0977778d6ea58ac06ddb9c1f
Dávid Varga <david.varga.7@student.tuke.sk>
2022-01-20 00:39:31 +0100

    add quick actions component to footer

--- frontend/tablet/src/screens/RoomDetailScreen/Footer/Footer.tsx
+++ frontend/tablet/src/screens/RoomDetailScreen/Footer/Footer.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
 
-import { Typography, Button, theme, Grid } from '../../../components'
+import { Grid } from '../../../components'
 import NextEvent from './NextEvent'
+import QuickActions from './QuickActions'
 
 type FooterProps = {
   currentRoomId: string
@@ -11,44 +11,10 @@ type FooterProps = {
 const Footer: React.FC<FooterProps> = ({ currentRoomId }) => {
   return (
     <Grid direction="row" justify="space-between" alignItems="flex-end">
-      <Grid justify="flex-end">
-        <Typography variant="button">QUICK RESERVATION</Typography>
-        <Grid direction="row" spacing={1} style={styles.buttons}>
-          <Button
-            title="15 min"
-            // eslint-disable-next-line no-console
-            onPress={() => console.log('TODO reserve 15 min')}
-          />
-          <Button
-            title="30 min"
-            // eslint-disable-next-line no-console
-            onPress={() => console.log('TODO reserve 30 min')}
-          />
-          <Button
-            title="45 min"
-            // eslint-disable-next-line no-console
-            onPress={() => console.log('TODO reserve 45 min')}
-          />
-          <Button
-            title="60 min"
-            // eslint-disable-next-line no-console
-            onPress={() => console.log('TODO reserve 60 min')}
-          />
-        </Grid>
-      </Grid>
-
+      <QuickActions currentRoomId={currentRoomId} />
       <NextEvent currentRoomId={currentRoomId} />
     </Grid>
   )
 }
 
 export default Footer
-
-const styles = StyleSheet.create({
-  buttons: {
-    marginTop: theme.spacing.lg,
-  },
-  meetingName: {
-    marginTop: theme.spacing.md,
-  },
-})
