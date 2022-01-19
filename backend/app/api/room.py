c6d46f90b85566db1aa7904ddade11cd861d8010
Mellowor <91839637+Mellowor@users.noreply.github.com>
2022-01-19 10:40:18 +0100

    Updated claims check

--- backend/app/api/room.py
+++ backend/app/api/room.py
@@ -9,36 +9,46 @@ room_bp = Blueprint("room_bp", __name__)
 
 @room_bp.route("/rooms", methods=["GET"])
 def get_room_list():
-    # TODO: check if tenant has permissions to view all rooms
-    url_args = request.args
-    params = room_service.url_args_to_query_params_dict(url_args)
-    results = room_dao.get_all(
-        params['filters'],
-        params['sort'], 
-        params['range'],
-        True if "with-events" in request.args else False,
-        True if "with-next-events" in request.args else False
-        )
-    resp = make_response(jsonify(results['data']), 200)
-    resp.headers['Access-Control-Expose-Headers'] = 'X-Total-Count'
-    resp.headers['X-Total-Count'] = results['count']
-    return resp
+    accessible_roles = ["*"]
+    returned_value = have_claims(request.headers.get("Authorization"),accessible_roles)
+    if returned_value[0]:
+        url_args = request.args
+        params = room_service.url_args_to_query_params_dict(url_args)
+        results = room_dao.get_all(
+            params['filters'],
+            params['sort'], 
+            params['range'],
+            True if "with-events" in request.args else False,
+            True if "with-next-events" in request.args else False
+            )
+        resp = make_response(jsonify(results['data']), 200)
+        resp.headers['Access-Control-Expose-Headers'] = 'X-Total-Count'
+        resp.headers['X-Total-Count'] = results['count']
+        return resp
+    return make_response(jsonify({}), 403)
 
 @room_bp.route("/rooms/<id>", methods=["GET"])
 def get_room_one(id):
-    #TODO: check if tenant has permissions to view desired room
-    return jsonify(room_dao.get_one(id))
+    accessible_roles = ["*"]
+    returned_value = have_claims(request.headers.get("Authorization"),accessible_roles)
+    if returned_value[0]:   
+        return jsonify(room_dao.get_one(id))
+    return make_response(jsonify({}), 403)
 
 @room_bp.route("/rooms/<id>", methods=["PUT"])
 def update_room(id):
-    if not have_claims(request.headers.get("Authorization"),"SUPER_ADMIN"):
+    accessible_roles = ["SUPER_ADMIN"]
+    returned_value = have_claims(request.headers.get("Authorization"),accessible_roles)
+    if not returned_value[0]:
         return make_response(jsonify({}), 403)
     data = request.json
     return jsonify(room_dao.update(id, data))
 
 @room_bp.route("/rooms/<id>", methods=["DELETE"])
 def delete_room(id):
-    if not have_claims(request.headers.get("Authorization"),"SUPER_ADMIN"):
+    accessible_roles = ["SUPER_ADMIN"]
+    returned_value = have_claims(request.headers.get("Authorization"),accessible_roles)
+    if not returned_value[0]:
         return make_response(jsonify({}), 403)
     room_dao.delete(id)
     return jsonify({})
@@ -46,7 +56,9 @@ def delete_room(id):
 
 @room_bp.route("/rooms", methods=["POST"])
 def create_room():
-    if not have_claims(request.headers.get("Authorization"),"SUPER_ADMIN"):
+    accessible_roles = ["SUPER_ADMIN"]
+    returned_value = have_claims(request.headers.get("Authorization"),accessible_roles)
+    if not returned_value[0]:
         return make_response(jsonify({}), 403)
     data = request.json
     new_room = room_dao.add(
