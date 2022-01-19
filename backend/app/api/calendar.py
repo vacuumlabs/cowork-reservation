fef602fce2552a9e3ef190b163bd0bc866adb444
Mellowor <91839637+Mellowor@users.noreply.github.com>
2022-01-19 10:41:26 +0100

    Updated claims check

--- backend/app/api/calendar.py
+++ backend/app/api/calendar.py
@@ -9,49 +9,60 @@ calendar_bp = Blueprint("calendar_bp", __name__)
 
 @calendar_bp.route("/calendars/", methods=["GET"])
 def get_calendar_list():
-    # TODO: check if tenant has permissions to view all calendars
-    url_args = request.args
-    params = calendar_service.url_args_to_query_params_dict(url_args)
-    results = calendar_dao.get_all(
-        params['filters'],
-        params['sort'], 
-        params['range']
-        )
-    resp = make_response(jsonify(results["data"]), 200)
-    resp.headers["Access-Control-Expose-Headers"] = "X-Total-Count"
-    resp.headers["X-Total-Count"] = results["count"]
-    return resp
-
+    accessible_roles = ["*"]
+    returned_value = have_claims(request.headers.get("Authorization"),accessible_roles)
+    if returned_value[0]:
+        url_args = request.args
+        params = calendar_service.url_args_to_query_params_dict(url_args)
+        results = calendar_dao.get_all(
+            params['filters'],
+            params['sort'], 
+            params['range']
+            )
+        resp = make_response(jsonify(results["data"]), 200)
+        resp.headers["Access-Control-Expose-Headers"] = "X-Total-Count"
+        resp.headers["X-Total-Count"] = results["count"]
+        return resp
+    return make_response(jsonify({}), 403)
 
 @calendar_bp.route("/calendars/<id>", methods=["GET"])
 def get_calendar(id):
-    # TODO: check if tenant has permissions to view desired calendar
-    return jsonify(calendar_dao.get_one(id))
+    accessible_roles = ["*"]
+    returned_value = have_claims(request.headers.get("Authorization"),accessible_roles)
+    if returned_value[0]:
+        return jsonify(calendar_dao.get_one(id))
+    return make_response(jsonify({}), 403)
 
 
 @calendar_bp.route("/calendars/<id>", methods=["PUT"])
 def update_calendar(id):
-    if not have_claims(request.headers.get("Authorization")):
-        return make_response(jsonify({}), 403)
-    data = request.json
-    return jsonify(calendar_dao.update(id, data))
+    accessible_roles = ["SUPER_ADMIN","TENANT_ADMIN"]
+    returned_value = have_claims(request.headers.get("Authorization"),accessible_roles)
+    if returned_value[0]:
+        data = request.json
+        return jsonify(calendar_dao.update(id, data))
+    return make_response(jsonify({}), 403)
 
 
 @calendar_bp.route("/calendars/<id>", methods=["DELETE"])
 def delete_calendar(id):
-    if not have_claims(request.headers.get("Authorization")):
-        return make_response(jsonify({}), 403)
-    calendar_dao.delete(id)
-    return jsonify({})
+    accessible_roles = ["SUPER_ADMIN","TENANT_ADMIN"]
+    returned_value = have_claims(request.headers.get("Authorization"),accessible_roles)
+    if returned_value[0]:
+        calendar_dao.delete(id)
+        return jsonify({})
+    return make_response(jsonify({}), 403)
 
 
 @calendar_bp.route("/calendars/", methods=["POST"])
 def create_calendar():
-    if not have_claims(request.headers.get("Authorization")):
-        return make_response(jsonify({}), 403)
-    data = request.json
-    new_calendar = calendar_dao.add(
-        int(data["tenant_id"]), data["name"], int(data["google_id"])
-    )
-    return jsonify(new_calendar)
+    accessible_roles = ["SUPER_ADMIN","TENANT_ADMIN"]
+    returned_value = have_claims(request.headers.get("Authorization"),accessible_roles)
+    if returned_value[0]:
+        data = request.json
+        new_calendar = calendar_dao.add(
+            int(data["tenant_id"]), data["name"], int(data["google_id"])
+        )
+        return jsonify(new_calendar)
+    return make_response(jsonify({}), 403)
     
