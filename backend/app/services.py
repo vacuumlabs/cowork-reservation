d3419adf59e058e99f4ab4c32df2bbefd4e53dcd
MichaelJ-T <59335490+MichaelJ-T@users.noreply.github.com>
2022-01-22 19:57:24 +0100

    Fixed sort issue and formatting in services

--- backend/app/services.py
+++ backend/app/services.py
@@ -1,30 +1,32 @@
 from array import array
 from app.daos import tenant_dao
 import json
-from app.utils import camel_to_snake_dict, snake_to_camel_dict 
+from app.utils import camel_to_snake_dict, snake_to_camel_dict, to_snake
 from flask import make_response, jsonify
 
 
 class SharedServices:
-    def url_args_to_query_params_dict_json(self, url_args, to_snake_case: bool = True): 
-        #For JSON data provider approach
+    def url_args_to_query_params_dict_json(self, url_args, to_snake_case: bool = True):
+        # For JSON data provider approach
         filters = {}
         sort = []
         results_range = []
         for key, value in url_args.items(multi=True):
             if key == "id":
                 if "id" in filters:
-                    filters['id'].append(int(value))
+                    filters["id"].append(int(value))
                 else:
-                    filters['id'] = [int(value)]
+                    filters["id"] = [int(value)]
             elif key == "_sort":
-                sort.insert(0,str(value))
+                sort.insert(0, str(value))
+                if to_snake_case:
+                    sort[0] = to_snake(sort[0])
             elif key == "_order":
-                sort.insert(1,str(value))
+                sort.insert(1, str(value))
             elif key == "_start":
-                results_range.insert(0,int(value))
+                results_range.insert(0, int(value))
             elif key == "_end":
-                results_range.insert(1,int(value))
+                results_range.insert(1, int(value))
             else:
                 if key in filters:
                     filters[key].append(value)
@@ -32,9 +34,9 @@ class SharedServices:
                     filters[key] = [value]
         parameters = {"filters": filters, "sort": sort, "range": results_range}
         return camel_to_snake_dict(parameters) if to_snake_case else parameters
-    
+
     def url_args_to_query_params_dict(self, url_args, to_snake_case: bool = True):
-        #For REST data provider approach
+        # For REST data provider approach
         filters = {}
         sort = []
         results_range = []
@@ -44,14 +46,22 @@ class SharedServices:
 
         if "sort" in url_args:
             sort = json.loads(url_args["sort"])
+            if to_snake_case:
+                sort[0] = to_snake(sort[0])
 
         if "range" in url_args:
-            results_range = json.loads(url_args["range"])    
+            results_range = json.loads(url_args["range"])
 
         parameters = {"filters": filters, "sort": sort, "range": results_range}
-        return camel_to_snake_dict(parameters) if to_snake_case else parameters 
+        return camel_to_snake_dict(parameters) if to_snake_case else parameters
 
-    def response(self, data: any = {}, content_range: int = 0, status_code: int = 200, to_camelCase: bool = True):
+    def response(
+        self,
+        data: any = {},
+        content_range: int = 0,
+        status_code: int = 200,
+        to_camelCase: bool = True,
+    ):
         if to_camelCase:
             if type(data) is list or type(data) is array:
                 for idx in range(len(data)):
@@ -64,9 +74,10 @@ class SharedServices:
         else:
             data_ready_to_send = jsonify(data)
         resp = make_response(data_ready_to_send, status_code)
-        resp.headers['Access-Control-Expose-Headers'] = 'Content-Range'
-        resp.headers['Content-Range'] = content_range
+        resp.headers["Access-Control-Expose-Headers"] = "Content-Range"
+        resp.headers["Content-Range"] = content_range
         return resp
+
 
 class TenantService(SharedServices):
     def to_lower_case_tenant(self, data: dict):
@@ -74,14 +85,18 @@ class TenantService(SharedServices):
         # example: lowercase all emails
         return data
 
+
 class EventService(SharedServices):
     pass
+
 
 class RoomService(SharedServices):
     pass
 
+
 class CalendarService(SharedServices):
     pass
+
 
 city_service = SharedServices()
 building_service = SharedServices()
